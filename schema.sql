6081beba4392837939bcb0b5f7a4c4d5c83af3c4
admin <peter.dyke1@gmail.com>
2021-08-11 23:11:46 -0400

    updated schema structure and imported data

--- schema.sql
+++ schema.sql
@@ -26,15 +26,6 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
 CREATE TABLE dept_emp (
 	emp_no INT NOT Null,
 	dept_no VARCHAR(4) NOT Null,
@@ -45,11 +36,28 @@ CREATE TABLE dept_emp (
 	PRIMARY KEY (emp_no, dept_no)
 );
 
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
 CREATE TABLE titles (
 	emp_no INT NOT Null,
 	title VARCHAR NOT Null,
 	from_date DATE NOT Null,
 	to_date DATE NOT Null,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)	
 );
+
+-- Drop TABLE departments cascade;
+-- drop table employees cascade;
+-- drop table dept_emp cascade;
+-- drop table dept_manager cascade;
+-- drop table salaries cascade;
+-- drop table titles cascade;
+
+select * from titles;
