f85093e83ce8828e7435d53bd0168ea2337fc481
admin <peter.dyke1@gmail.com>
2021-08-15 13:15:08 -0400

    Added queries document and retirement_info csv file

---
+++ queries.sql
@@ -0,0 +1,24 @@
+-- Select All employees born btw 1952 and 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Select All employees born in 1952, 53, 54, 55
+Select first_name, last_name From employees WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+Select first_name, last_name From employees WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+Select first_name, last_name From employees WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+Select first_name, last_name From employees WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement Eligibility
+SELECT count(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Build Retirement_info table with above data
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+select * from retirement_info;
